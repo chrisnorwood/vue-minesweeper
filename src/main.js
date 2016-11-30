535e2b881c61d1e6ff6ea25dff9e7115678bd15e
Chris Norwood <cnorwood92@gmail.com>
2016-11-30 09:12:06 -0800

    Remove unnecessary vue-resource dependency

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import Vue from 'vue';
-import VueResource from 'vue-resource';
-
 import App from './App';
 import './styles/global.scss';
-
-Vue.use(VueResource);
-Vue.http.options.root = 'https://vue-minesweeper.firebaseio.com';
 
 /* eslint-disable no-new */
 new Vue({
